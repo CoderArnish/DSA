d87d63a94a8e5d046ad3b9233add0212d66e5563
CoderArnish <aa6908@srmist.edu.in>
2024-03-10 18:45:29 +0530

    Linked list insertion at the end

---
+++ Linked_list/insertion_end.py
@@ -0,0 +1,35 @@
+#Insertion in Linked List at End
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList():
+    def __init__(self):
+        self.head=None
+        
+    def insertAtEnd(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+            return
+        
+        current_node= self.head
+        while(current_node.next):
+            current_node=current_node.next
+        
+        current_node.next=new_node
+
+    def printLL(self):
+        current_node=self.head
+        while(current_node):
+            print(current_node.data)
+            current_node=current_node.next
+
+lis=LinkedList()
+lis.insertAtEnd('a')
+lis.insertAtEnd('b')
+lis.insertAtEnd('c')
+lis.insertAtEnd('d')
+lis.printLL()
